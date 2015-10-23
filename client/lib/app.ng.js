1b343ad0c165f156d591277f2ce2746a922a8586
gordon peterson <gordon.peterson@imail.org>
2015-10-23 02:26:48 -0600

    re-created app.js to use style guide

--- client/lib/app.ng.js
+++ client/lib/app.ng.js
@@ -1,20 +1,38 @@
-/* jshint ignore:start */
-angular.module('motodynastyApp', [
-  'angular-meteor',
-  'ngAnimate', 
-  'ionic',
-  'ui.router'
-]);
+( function () {
+'use strict';
+	
+	/**
+	 * @ngdoc function
+	 * @name motodynastyApp
+	 * @description
+	 * The motodynastyApp module. 
+	 */
 
-onReady = function() {
-  angular.bootstrap(document, ['motodynastyApp']);
-};
-  
-if(Meteor.isCordova) {
-  angular.element(document).on('deviceready', onReady);
-} else {
-  angular.element(document).ready(onReady);
-}
-/* jshint ignore:end */
+		angular
+		.module('motodynastyApp', [
+			'angular-meteor',
+			'ngAnimate', 
+			'ionic',
+			'ui.router'
+		])
+		
+		.config( function() {
+			console.log('motodynastyApp config()')
+		})
+		
+		.run( function() {
+			console.log('motodynastyApp run()')
+		})
 
+		if(Meteor.isCordova) {
+			angular.element(document).on('deviceready', onReady);
+		} else {
+			angular.element(document).ready(onReady);
+		}
+
+		function onReady ( event ) {
+			angular.bootstrap(document, ['motodynastyApp']);
+		}
+
+})(); 
 
