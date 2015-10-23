6f801333668252d297900cfcb813cca73146d2fa
gordon peterson <gordon.peterson@imail.org>
2015-10-23 01:03:10 -0600

    added iffe back to controller

--- client/account/accountCtrl.ng.js
+++ client/account/accountCtrl.ng.js
@@ -1,41 +1,45 @@
-'use strict';
-
-/**
- * @ngdoc function
- * @name motodynastyApp:accountCtrl
- * @description
- * # accountCtrl
- * The accountCtrl controller for motodynastyApp module. 
- */
-
-angular
-	.module('motodynastyApp')
-	.controller('accountCtrl', accountCtrl);
-
-/* @ngInject */
-function accountCtrl ( $scope, $rootScope, $meteor ) {
-
-	//jshint ignore:line
-	var vm = this; 
-	vm.controllerName = 'accountCtrl';
-	vm.username = '';
-	vm.isAdmin = false;
+( function () {
+	
+	'use strict';
+	
+	/**
+	 * @ngdoc function
+	 * @name motodynastyApp:accountCtrl
+	 * @description
+	 * # accountCtrl
+	 * The accountCtrl controller for motodynastyApp module. 
+	 */
+	
+	angular
+		.module('motodynastyApp')
+		.controller('accountCtrl', accountCtrl);
+	
+	/* @ngInject */
+	function accountCtrl ( $scope, $rootScope, $meteor ) {
+	
+		//jshint ignore:line
+		var vm = this; 
+		vm.controllerName = 'accountCtrl';
+		vm.username = '';
+		vm.isAdmin = false;
+		
+	
+		activate();
+	
+		function activate () {
+			console.log( 'activated controller: ' + vm.controllerName );
+	
+			var user = $rootScope.currentUser;
+	
+			if( user ){
+				vm.username = user.username;
+				vm.isAdmin = window.isAdmin();
+	
+			}
+	
+		}
+	
+	}
 	
 
-	activate();
-
-	function activate () {
-		console.log( 'activated controller: ' + vm.controllerName );
-
-		var user = $rootScope.currentUser;
-
-		if( user ){
-			vm.username = user.username;
-			vm.isAdmin = window.isAdmin();
-
-		}
-
-	}
-
-}
-
+})();
